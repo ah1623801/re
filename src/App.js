fb03210526695115bf80ddcdd6f9ded393ef078b
ah1623801 <99343837+ah1623801@users.noreply.github.com>
2024-07-14 09:20:02 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import {Routes, Route, Link} from "react-router-dom";
-import Home from "./pages/home";
-import Html from "./pages/html";
-import Css from "./pages/css";
-import Javascript from "./pages/javascript";
-
-
-
-import React from 'react';
-
-function App(){
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Html" element={<Html />} />
-                <Route path="/Css" element={<Css />} />
-                <Route path="/Javascript" element={<Javascript />} />
-            </Routes>
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      App.js file
+    </div>
+  );
 }
 
 export default App;
-
